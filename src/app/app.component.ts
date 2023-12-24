57e5cb973285f453204210d3fe5a4150b7733bd3
DESKTOP-09OGCGH\gilcl <gilcllyscosta@gmail.com>
2023-12-24 12:25:39 -0400

    login and register screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router'; 
+import {MatTabsModule} from '@angular/material/tabs';
+import { LoginComponent } from "./components/login/login.component"; 
+import { RegisterComponent } from './components/register/register.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        MatTabsModule,
+        LoginComponent,
+        RegisterComponent
+    ]
 })
 export class AppComponent {
   title = 'football-rent-web';
+  label = {
+    Login: 'Entrar',
+    Register: 'Cadastrar'
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatCardModule} from '@angular/material/card'; 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
